9bdda86011a16285723edc4bf53ee73f26cc8fc3
mosheco110 <mosheco110@gmail.com>
2019-01-07 20:20:03 +0200

    all

--- wpf_MVVM_EntityFramework/MainWindow.xaml.cs
+++ wpf_MVVM_EntityFramework/MainWindow.xaml.cs
@@ -111,6 +111,17 @@ namespace wpf_MVVM_EntityFramework
         {
            Main.Content = new UserControls.CollerCenter();
         }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new UserControls.C1maps();
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+        
+        }
     }
 }
     
--- wpf_MVVM_EntityFramework/UserControls/DropsMap.xaml.cs
+++ wpf_MVVM_EntityFramework/UserControls/DropsMap.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Drawing;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Wpf;
 
 namespace wpf_MVVM_EntityFramework.UserControls
 {
@@ -24,7 +30,12 @@ namespace wpf_MVVM_EntityFramework.UserControls
         {
 
             InitializeComponent();
-            WebBrowserGoogleMap.Navigate("https://www.google.com/maps?q=Shahal+Jerusalem+");
+            myMap.Focus();
+            //Set map to Aerial mode with labels
+            myMap.Mode = new AerialMode(true);
+
+
+           WebBrowserGoogleMap.Navigate("https://www.google.com/maps?q=Shahal+Jerusalem+") ;  
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -37,8 +48,14 @@ namespace wpf_MVVM_EntityFramework.UserControls
             try
             {
                 StringBuilder quarryAddress = new StringBuilder();
+                StringBuilder quarryAddress2 = new StringBuilder();
+                StringBuilder quarryAddress3 = new StringBuilder();
 
                 quarryAddress.Append("https://www.google.com/maps?q=");
+                //quarryAddress2.Append(" https://www.google.com/maps/d/u/0/edit?hl=iw&hl=iw&mid=1Cbdx-P-iEr0iTcSYEH3sno3gUsG0WMA2&ll=31.805111156673668%2C34.964537153527544&z=11 ");
+                //quarryAddress3.Append(" https://www.google.com/maps/d/u/0/edit?hl=iw&hl=iw&mid=1Cbdx-P-iEr0iTcSYEH3sno3gUsG0WMA2&ll=31.960262760614995%2C35.34942100000001&z=15 ");
+   
+              
                 //street
                 if (street != string.Empty)
                 {
@@ -66,5 +83,36 @@ namespace wpf_MVVM_EntityFramework.UserControls
                 MessageBox.Show(ex.Message.ToString(), "ERROR");
             }
         }
+
+        //private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    WpfMap.MapUnit = GeographyUnit.DecimalDegree;
+        //    WpfMap.CurrentExtent = new RectangleShape(-155.733, 95.60, 104.42, -81.9);
+
+        //    BackgroundLayer backgroundLayer = new BackgroundLayer(new GeoSolidBrush(GeoColor.GeographicColors.ShallowOcean));
+
+        //    ShapeFileFeatureLayer worldLayer = new ShapeFileFeatureLayer(@"..\..\SampleData\Data\Countries02.shp");
+        //     WpfMap.Refresh();  worldLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = ThinkGeo.MapSuite.Styles.AreaStyles.Country1;
+        //    WpfMap.Refresh();  worldLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = ThinkGeo.MapSuite.Styles.WorldStreetsAndAryaStyles;
+        //     WpfMap.Refresh(); worldLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = ThinkGeo.MapSuite.Wo;
+        //    worldLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+        //    LayerOverlay worldOverlay = new LayerOverlay();
+        //    worldOverlay.Layers.Add(backgroundLayer);
+        //    worldOverlay.Layers.Add(worldLayer);
+        //    WpfMap.Overlays.Add(worldOverlay);
+        //    WpfMap.Refresh();
+
+        //    /*  WpfMap.Refresh();  
+        //      WpfMap.MapUnit = GeographyUnit.DecimalDegree;
+        //      WpfMap.CurrentExtent = new RectangleShape(-155.733, 95.60, 104.42, -81.9);
+
+        //      //WpfMap.Overlays.Add( new WorldStreetsAndImageryOverlay());
+        //      WpfMap.Overlays.Add( new ThinkGeoCloudMapsOverlay());
+
+        //      WpfMap.Refresh();
+        //      */
+
+        //}
     }
 }
